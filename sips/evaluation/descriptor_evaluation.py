fix!: erase nan values in backprop step

sips/evaluation/descriptor_evaluation.py
@@ -137,8 +137,8 @@ def compute_matching_score_batch(
 
         # Match the descriptors from 1 to 2
         matches = bf.match(desc_1b.cpu().numpy(), desc_2b.cpu().numpy())
-        matches_idx1 = torch.tensor([m.queryIdx for m in matches])
-        matches_idx2 = torch.tensor([m.trainIdx for m in matches])
+        matches_idx1 = torch.tensor([m.queryIdx for m in matches]).long()
+        matches_idx2 = torch.tensor([m.trainIdx for m in matches]).long()
         m_coord_1b = coord_1b[matches_idx1, :]
         m_coord_2b = coord_2b[matches_idx2, :]
         # Warp the keypoints and keep only those which are visible in the other image
@@ -154,8 +154,8 @@ def compute_matching_score_batch(
 
         # Match the descriptors from 2 to 1
         matches = bf.match(desc_2b.cpu().numpy(), desc_1b.cpu().numpy())
-        matches_idx2 = torch.tensor([m.queryIdx for m in matches])
-        matches_idx1 = torch.tensor([m.trainIdx for m in matches])
+        matches_idx2 = torch.tensor([m.queryIdx for m in matches]).long()
+        matches_idx1 = torch.tensor([m.trainIdx for m in matches]).long()
         m_coord_1b = coord_1b[matches_idx1, :]
         m_coord_2b = coord_2b[matches_idx2, :]
         # Warp the keypoints and keep only those which are visible in the other image

sips/models/keypoint_net_with_io_loss.py
@@ -13,7 +13,7 @@ from sips.data import SonarBatch
 from sips.evaluation import evaluate_keypoint_net
 from sips.networks import InlierNet, KeypointNet, KeypointResnet
 from sips.utils.image import normalize_2d_coordinate, to_color_normalized
-from sips.utils.keypoint_matching import match_keypoints_2d_batch
+from sips.utils.keypoint_matching import PSEUDO_INF, match_keypoints_2d_batch
 from sips.utils.point_projection import warp_image_batch
 
 
@@ -74,7 +74,8 @@ def build_descriptor_loss(
         ).squeeze()
         tar_desc = F.grid_sample(
             target_des[b].unsqueeze(0),
-            tar_points[b].unsqueeze(0),
+            # convert nan to large value such that the gradient does not get nan
+            tar_points[b].unsqueeze(0).nan_to_num(PSEUDO_INF),
             align_corners=True,
         ).squeeze()
         tar_points_raw = tar_points_un[b]
@@ -323,9 +324,11 @@ class KeypointNetwithIOLoss(pl.LightningModule):
         usp_loss = 0.5 * (target_score_associated + source_score.flatten(1)[mask])
         usp_loss *= min_distance[mask] - min_distance[mask].mean()
 
+        # Fill nan values with dummy values such that the gradient does not get nan.
+        # Note that the nan-filled values anyway get masked out
         target_score_resampled = F.grid_sample(
             target_score,
-            source_uv_warp_norm.detach(),
+            source_uv_warp_norm.detach().nan_to_num(PSEUDO_INF),
             mode="bilinear",
             align_corners=True,
         ).view(B, HC * WC)

sips/utils/keypoint_matching.py
@@ -10,6 +10,9 @@ __all__ = ["match_keypoints_2d", "match_keypoints_2d_batch"]
 # Utils
 
 
+PSEUDO_INF = 1e6
+
+
 def _unravel_index_2d(
     flat_index: torch.Tensor, shape: Iterable[int]
 ) -> tuple[torch.Tensor, torch.Tensor]:
@@ -124,14 +127,9 @@ def _project_point_on_linesegment(
     # It falls where t = [(p-v) . (w-v)] / |w-v|^2
     # We clamp t from [0,1] to handle points outside the segment vw.
     l2 = torch.sum((w - v) ** 2, dim=-1)  # length squared -> |w-v|^2
+    l2 = l2.clamp(1e-6)  # handle v == w case
     t = (torch.einsum("...i,...i->...", p - v, w - v) / l2).clamp(0, 1)  # dot product
     projection = v + t[..., None] * (w - v)
-
-    # Handle v == w case
-    mask = l2 == 0
-    if mask.any():
-        mask = torch.broadcast_to(mask, projection.shape[:-1])
-        projection[mask] = v[mask]
 
     # Put dimension back to original place
     if dim not in (-1, v.ndim - 1):
@@ -147,7 +145,16 @@ def _point2point_distance(
     Calculates the Point-to-Point distances between ``p1`` and ``p2``.
 
     """
-    return torch.sum((p1 - p2) ** 2, dim=dim) ** 0.5
+    # Fill nan values with dummy values such that the gradient does not get nan
+    # then fill those dummy distances with nan values to obtain true distance.
+    nan_mask = ~(torch.isfinite(p1).all(dim) & torch.isfinite(p2).all(dim))
+    diff = p1.nan_to_num(PSEUDO_INF) - p2.nan_to_num(-PSEUDO_INF)
+    _ = diff**2
+    _ = torch.sum(_, dim=dim)
+    distance = _**0.5
+    # distance = torch.linalg.vector_norm(diff, dim=dim)
+    distance[nan_mask] = torch.nan
+    return distance
 
 
 def _point2linesegment_distance(
@@ -159,8 +166,19 @@ def _point2linesegment_distance(
     Every point ``p`` is projected onto the line segments ``v``-``w``.
 
     """
-    projection = _project_point_on_linesegment(v, w, p, dim=dim)
-    return _point2point_distance(p, projection, dim=dim)
+    # Fill nan values with dummy values such that the gradient does not get nan
+    # then fill those dummy distances with nan values to obtain true distance.
+    nan_mask = ~(
+        torch.isfinite(v).all(dim)
+        & torch.isfinite(w).all(dim)
+        & torch.isfinite(p).all(dim)
+    )
+    projection = _project_point_on_linesegment(
+        v.nan_to_num(), w.nan_to_num(), p.nan_to_num(), dim=dim
+    )
+    distance = _point2point_distance(p, projection, dim=dim)
+    distance[nan_mask] = torch.nan
+    return distance
 
 
 # ==============================================================================
@@ -202,10 +220,9 @@ def _point2point_match_batch(
         .take_along_dim(batch_idx[None, :, None, None, None], 0)
         .squeeze(0),
         dim=1,
-    )
-    masked_dists = masked_dists.nan_to_num(torch.inf).view(-1, WS**2)
+    ).view(-1, WS**2)
     distances = torch.full((*kp2_mask.shape, WS**2), torch.inf, device=device)
-    distances[kp2_mask] = masked_dists
+    distances[kp2_mask] = masked_dists.nan_to_num(torch.inf)
 
     # Find the closest points
     # Flatten such that we get a unique argmin for every set of projected keypoints in 2